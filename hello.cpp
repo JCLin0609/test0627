19c841191533f602eb09d817992a61556fc96378
JCLin0609 <as11675847@gmail.com>
2022-06-27 16:21:58 +0800

    testFunction*3

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void testfunc(){
+void testfunc2(){
+    cout << "Hello world." << endl;
+    cout << "Hello world." << endl;
     cout << "Hello world." << endl;
     return;
 }
 
 int main(){
-    testfunc();
+    testfunc2();
     return 0;
 }
